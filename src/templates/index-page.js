fcb591d7dd4ab4d1752736661c92a361aab6a3b3
Emmett Hoolahan <emmetthoolahan@gmail.com>
2020-08-03 23:05:12 +0800

    Update index-page.js
    j

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -50,7 +50,7 @@ export const IndexPageTemplate = ({
           {title}
         </h1>
         <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-12-widescreen"
           style={{
             boxShadow:
               'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
@@ -96,7 +96,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Latest storieszz
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
